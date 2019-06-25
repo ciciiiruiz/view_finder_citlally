39ee7088ff7dcae03694a6832af12afb11c65b16
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-25 09:59:18 -0500

    Initial Commit

---
+++ view_finder_citlally/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  view_finder_citlally
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Citlally Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
